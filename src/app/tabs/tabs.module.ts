d721282e8639ed8b5d3d8dc1cf860aa15b6d6443
Kasra Koushan <kasra.koushan@gmail.com>
2019-07-12 00:13:08 +0100

    Setup tab interface by modifying tabs.page.html and tabs.module.ts

--- src/app/tabs/tabs.module.ts
+++ src/app/tabs/tabs.module.ts
@@ -9,8 +9,17 @@ import { TabsPage } from './tabs.page';
 
 const routes: Routes = [
   {
+    path: 'tabs',
+    component: TabsPage,
+    children: [
+      {path: 'tab1', loadChildren: '../tab1/tab1.module#Tab1PageModule'},
+      {path: 'tab2', loadChildren: '../tab2/tab2.module#Tab2PageModule'}
+    ]
+  },
+  {
     path: '',
-    component: TabsPage
+    redirectTo: 'tabs/tab1',
+    pathMatch: 'full'
   }
 ];
 
